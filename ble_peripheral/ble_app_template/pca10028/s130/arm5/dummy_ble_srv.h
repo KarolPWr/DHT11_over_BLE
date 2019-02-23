a94da751ae2828da47e5a79b1f4edf193ff92425
KarolPWr <213952@student.pwr.edu.pl>
2019-02-23 20:52:59 +0100

    entry point for implementation

--- ble_peripheral/ble_app_template/pca10028/s130/arm5/dummy_ble_srv.h
+++ ble_peripheral/ble_app_template/pca10028/s130/arm5/dummy_ble_srv.h
@@ -1,62 +1,133 @@
-#ifndef DUMMY_BLE_SRV_H__
-#define DUMMY_BLE_SRV_H__
+/* Copyright (c) 2012 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+/** @file
+ *
+ * @defgroup ble_sdk_srv_bas Battery Service
+ * @{
+ * @ingroup ble_sdk_srv
+ * @brief Battery Service module.
+ *
+ * @details This module implements the Battery Service with the Battery Level characteristic.
+ *          During initialization it adds the Battery Service and Battery Level characteristic
+ *          to the BLE stack database. Optionally it can also add a Report Reference descriptor
+ *          to the Battery Level characteristic (used when including the Battery Service in
+ *          the HID service).
+ *
+ *          If specified, the module will support notification of the Battery Level characteristic
+ *          through the ble_bas_battery_level_update() function.
+ *          If an event handler is supplied by the application, the Battery Service will
+ *          generate Battery Service events to the application.
+ *
+ * @note The application must propagate BLE stack events to the Battery Service module by calling
+ *       ble_bas_on_ble_evt() from the @ref softdevice_handler callback.
+ *
+ * @note Attention! 
+ *  To maintain compliance with Nordic Semiconductor ASA Bluetooth profile 
+ *  qualification listings, this section of source code must not be modified.
+ */
+
+#ifndef BLE_BAS_H__
+#define BLE_BAS_H__
 
 #include <stdint.h>
 #include <stdbool.h>
 #include "ble.h"
 #include "ble_srv_common.h"
 
-#define LBS_UUID_BASE {0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, 0xDE, 0xEF, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00}
-#define LBS_UUID_SERVICE 0x1523
-#define LBS_UUID_LED_CHAR 0x1525
-#define LBS_UUID_BUTTON_CHAR 0x1524
+/**@brief Battery Service event type. */
+typedef enum
+{
+    BLE_BAS_EVT_NOTIFICATION_ENABLED,                             /**< Battery value notification enabled event. */
+    BLE_BAS_EVT_NOTIFICATION_DISABLED                             /**< Battery value notification disabled event. */
+} ble_bas_evt_type_t;
 
-// Forward declaration of the ble_dht_t type. 
-typedef struct ble_dht_s ble_dht_t;
-
-typedef void (*ble_dht_write_handler_t) (ble_dht_t * p_dht, uint8_t new_state);
-
+/**@brief Battery Service event. */
 typedef struct
 {
-    ble_dht_write_handler_t dht_write_handler;                    /**< Event handler to be called when dht characteristic is written. */
-} ble_dht_init_t;
+    ble_bas_evt_type_t evt_type;                                  /**< Type of event. */
+} ble_bas_evt_t;
 
-/**@brief DHT Service structure. This contains various status information for the service. */
-typedef struct ble_dht_s
+// Forward declaration of the ble_bas_t type. 
+typedef struct ble_bas_s ble_bas_t;
+
+/**@brief Battery Service event handler type. */
+typedef void (*ble_bas_evt_handler_t) (ble_bas_t * p_bas, ble_bas_evt_t * p_evt);
+
+/**@brief Battery Service init structure. This contains all options and data needed for
+ *        initialization of the service.*/
+typedef struct
 {
-    uint16_t                    service_handle;
-    ble_gatts_char_handles_t    dht_handles;
-    //ble_gatts_char_handles_t    button_char_handles;
-    uint8_t                     uuid_type;
-    uint16_t                    conn_handle;
-    ble_dht_write_handler_t dht_write_handler;
-} ble_dht_t;
+    ble_bas_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the Battery Service. */
+    bool                          support_notification;           /**< TRUE if notification of Battery Level measurement is supported. */
+    ble_srv_report_ref_t *        p_report_ref;                   /**< If not NULL, a Report Reference descriptor with the specified value will be added to the Battery Level characteristic */
+    uint8_t                       initial_batt_level;             /**< Initial battery level */
+    ble_srv_cccd_security_mode_t  battery_level_char_attr_md;     /**< Initial security level for battery characteristics attribute */
+    ble_gap_conn_sec_mode_t       battery_level_report_read_perm; /**< Initial security level for battery report read attribute */
+} ble_bas_init_t;
 
-/**@brief Function for initializing the LED Button Service.
+/**@brief Battery Service structure. This contains various status information for the service. */
+struct ble_bas_s
+{
+    ble_bas_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the Battery Service. */
+    uint16_t                      service_handle;                 /**< Handle of Battery Service (as provided by the BLE stack). */
+    ble_gatts_char_handles_t      battery_level_handles;          /**< Handles related to the Battery Level characteristic. */
+    uint16_t                      report_ref_handle;              /**< Handle of the Report Reference descriptor. */
+    uint8_t                       battery_level_last;             /**< Last Battery Level measurement passed to the Battery Service. */
+    uint16_t                      conn_handle;                    /**< Handle of the current connection (as provided by the BLE stack, is BLE_CONN_HANDLE_INVALID if not in a connection). */
+    bool                          is_notification_supported;      /**< TRUE if notification of Battery Level is supported. */
+};
+
+/**@brief Function for initializing the Battery Service.
  *
- * @param[out]  p_dht       DHT structure. This structure will have to be supplied by
+ * @param[out]  p_bas       Battery Service structure. This structure will have to be supplied by
  *                          the application. It will be initialized by this function, and will later
  *                          be used to identify this particular service instance.
- * @param[in]   p_dht_init  Information needed to initialize the service.
+ * @param[in]   p_bas_init  Information needed to initialize the service.
  *
  * @return      NRF_SUCCESS on successful initialization of service, otherwise an error code.
  */
-uint32_t ble_dht_init(ble_dht_t * p_dht, const ble_dht_init_t * p_dht_init);
+uint32_t ble_bas_init(ble_bas_t * p_bas, const ble_bas_init_t * p_bas_init);
 
 /**@brief Function for handling the Application's BLE Stack events.
  *
- * @details Handles all events from the BLE stack of interest to the LED Button Service.
+ * @details Handles all events from the BLE stack of interest to the Battery Service.
  *
+ * @note For the requirements in the BAS specification to be fulfilled,
+ *       ble_bas_battery_level_update() must be called upon reconnection if the
+ *       battery level has changed while the service has been disconnected from a bonded
+ *       client.
  *
- * @param[in]   p_dht      LED Button Service structure.
+ * @param[in]   p_bas      Battery Service structure.
  * @param[in]   p_ble_evt  Event received from the BLE stack.
  */
-void ble_dht_on_ble_evt(ble_dht_t * p_dht, ble_evt_t * p_ble_evt);
+void ble_bas_on_ble_evt(ble_bas_t * p_bas, ble_evt_t * p_ble_evt);
 
-/**@brief Function for sending a sensor reading notification.
+/**@brief Function for updating the battery level.
+ *
+ * @details The application calls this function after having performed a battery measurement. If
+ *          notification has been enabled, the battery level characteristic is sent to the client.
+ *
+ * @note For the requirements in the BAS specification to be fulfilled,
+ *       this function must be called upon reconnection if the battery level has changed
+ *       while the service has been disconnected from a bonded client.
+ *
+ * @param[in]   p_bas          Battery Service structure.
+ * @param[in]   battery_level  New battery measurement value (in percent of full capacity).
+ *
+ * @return      NRF_SUCCESS on success, otherwise an error code.
  */
-uint32_t ble_dht_on_reading_change(ble_dht_t * p_dht, uint8_t dht_state);
+uint32_t ble_bas_battery_level_update(ble_bas_t * p_bas, uint8_t battery_level);
 
+#endif // BLE_BAS_H__
 
-
-#endif
+/** @} */
