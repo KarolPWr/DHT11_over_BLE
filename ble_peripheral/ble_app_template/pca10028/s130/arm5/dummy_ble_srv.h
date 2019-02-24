c40f59c8bfa6a47974eeca68b1a51213cf539dc5
KarolPWr <213952@student.pwr.edu.pl>
2019-02-24 15:11:24 +0100

    Finished next steps, added struct definitions and deleted battery event declarations

--- ble_peripheral/ble_app_template/pca10028/s130/arm5/dummy_ble_srv.h
+++ ble_peripheral/ble_app_template/pca10028/s130/arm5/dummy_ble_srv.h
@@ -44,48 +44,28 @@
 #include "ble.h"
 #include "ble_srv_common.h"
 
-/**@brief Battery Service event type. */
-typedef enum
-{
-    ble_dht_EVT_NOTIFICATION_ENABLED,                             /**< Battery value notification enabled event. */
-    ble_dht_EVT_NOTIFICATION_DISABLED                             /**< Battery value notification disabled event. */
-} ble_dht_evt_type_t;
-
-/**@brief Battery Service event. */
-typedef struct
-{
-    ble_dht_evt_type_t evt_type;                                  /**< Type of event. */
-} ble_dht_evt_t;
-
 // Forward declaration of the ble_dht_t type. 
 typedef struct ble_dht_s ble_dht_t;
 
 /**@brief Battery Service event handler type. */
-typedef void (*ble_dht_evt_handler_t) (ble_dht_t * p_dht, ble_dht_evt_t * p_evt);
+typedef void (*ble_dht_evt_handler_t) (ble_dht_t * p_dht, uint8_t new_state);
 
 /**@brief Battery Service init structure. This contains all options and data needed for
  *        initialization of the service.*/
 typedef struct
 {
-    ble_dht_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the Battery Service. */
-    bool                          support_notification;           /**< TRUE if notification of Battery Level measurement is supported. */
-    ble_srv_report_ref_t *        p_report_ref;                   /**< If not NULL, a Report Reference descriptor with the specified value will be added to the Battery Level characteristic */
-    uint8_t                       initial_batt_level;             /**< Initial battery level */
-    ble_srv_cccd_security_mode_t  battery_level_char_attr_md;     /**< Initial security level for battery characteristics attribute */
-    ble_gap_conn_sec_mode_t       battery_level_report_read_perm; /**< Initial security level for battery report read attribute */
+    ble_dht_evt_handler_t         dht_write_handler;                    /**< Event handler to be called for handling events in the Battery Service. */
 } ble_dht_init_t;
 
 /**@brief Battery Service structure. This contains various status information for the service. */
-struct ble_dht_s
+typedef struct ble_dht_s
 {
-    ble_dht_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the Battery Service. */
     uint16_t                      service_handle;                 /**< Handle of Battery Service (as provided by the BLE stack). */
-    ble_gatts_char_handles_t      battery_level_handles;          /**< Handles related to the Battery Level characteristic. */
-    uint16_t                      report_ref_handle;              /**< Handle of the Report Reference descriptor. */
-    uint8_t                       battery_level_last;             /**< Last Battery Level measurement passed to the Battery Service. */
+    ble_gatts_char_handles_t      dht_char_handles;          /**< Handles related to the Battery Level characteristic. */
+	  uint8_t                       uuid_type;
     uint16_t                      conn_handle;                    /**< Handle of the current connection (as provided by the BLE stack, is BLE_CONN_HANDLE_INVALID if not in a connection). */
-    bool                          is_notification_supported;      /**< TRUE if notification of Battery Level is supported. */
-};
+	  ble_dht_evt_handler_t         dht_write_handler; 
+} ble_dht_t;
 
 /**@brief Function for initializing the Battery Service.
  *
